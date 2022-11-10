a9995af1cdaea22bc991685912df70f7c8f89920
Kananda Ribas <kanandaribas@gmail.com>
2022-11-10 18:23:28 -0300

    Requisito 06 -Concluido

---
+++ src/services/requestQuestionsApi.js
@@ -0,0 +1,9 @@
+const requestQuestionsApi = async () => {
+  const token = localStorage.getItem('token');
+  const url = `https://opentdb.com/api.php?amount=5&token=${token}`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+};
+
+export default requestQuestionsApi;
