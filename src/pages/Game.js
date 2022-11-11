a8255ee193b74274d91a1f839588c975065e52ea
Karla Oshikawa <karla_hara@hotmail.com>
2022-11-12 05:51:49 +0900

    Grupo 6-req 8 Karla-Saulo-Fabio-Kananda com o help do Dota-refatorado

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -9,54 +9,39 @@ class Game extends Component {
     questions: [],
     index: 0,
     color: false,
-    timer: 31,
+    timer: 30,
   };
 
   componentDidMount() {
     this.validToken();
     const second = 1000;
     // const interval = setInterval(this.handleTimer, second);
-    this.interval = window.setInterval(() => {
+    const interval = window.setInterval(() => {
       const { timer } = this.state;
-      const zero = 1;
+      const zero = 0;
       if (timer === zero) {
         this.setState({ color: true });
+        return window.clearInterval(interval);
       }
       this.setState((prev) => ({ timer: prev.timer - 1 }));
     }, second);
   }
 
-  componentDidUpdate() {
-    const { timer } = this.state;
-    const zero = 0;
-    if (timer === zero) {
-      clearInterval(this.interval);
-    }
-  }
-
-  // timerStop = () => {
-  //   const { timer } = this.state;
-  //   const second = 1000;
-  //   const interval = setInterval(() => {
-  //     this.setState((prev) => ({
-  //       timer: prev.timer - 1,
-  //     }), () => {
-  //       if (timer === 0) {
-  //         clearInterval(interval);
-  //       }
-  //     });
-  //   }, second);
-  // };
-
   validToken = async () => {
     const { history } = this.props;
     const result = await requestQuestionsApi();
+    const questions = result.results.map((perguntas) => {
+      const respostas = this
+        .questionRender([...perguntas.incorrect_answers, perguntas.correct_answer]);
+      const obj = { ...perguntas, respostas };
+      return obj;
+    });
     if (result.results.length === 0) {
       history.push('/');
       localStorage.removeItem('token');
     }
     this.setState({
-      questions: result.results,
+      questions,
     });
   };
 
@@ -68,11 +53,10 @@ class Game extends Component {
 
   render() {
     const { questions, index, color, timer } = this.state;
-    const zero = 0;
     return (
       <div>
         <Header />
-        {timer === zero ? (<h3>0</h3>) : (<h3>{ timer }</h3>)}
+        <h3>{ timer }</h3>
         { questions.map((element, i) => {
           if (i === index) {
             return (
@@ -80,9 +64,7 @@ class Game extends Component {
                 <h4 data-testid="question-category">{element.category}</h4>
                 <h3 data-testid="question-text">{element.question}</h3>
                 <div data-testid="answer-options">
-                  {this.questionRender(
-                    [...element.incorrect_answers, element.correct_answer],
-                  ).map((e, indexBtn) => {
+                  {element.respostas.map((e, indexBtn) => {
                     const btnColor = (e === element.correct_answer ? 'green' : 'red');
                     return (
                       <button
