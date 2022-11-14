e472089e83ff9ff0efd36567bc4400a063e6d896
Saulo Vasconcelos <saulovsf@gmail.com>
2022-11-14 13:55:06 -0400

    Requisito 19

--- src/services/handleLocalStorage.js
+++ src/services/handleLocalStorage.js
@@ -1,5 +1,12 @@
-const tokenStorage = (param) => {
+export const tokenStorage = (param) => {
   localStorage.setItem('token', param);
 };
 
-export default tokenStorage;
+export const playerStorage = (player) => {
+  localStorage.setItem('players', JSON.stringify(player));
+};
+
+export const getStorage = () => {
+  const obj = localStorage.getItem('players');
+  return JSON.parse(obj);
+};
